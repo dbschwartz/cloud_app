6866327a2f60c7701930d460ae75994c264a41fc
David Schwartz <dbschwartz2@gmail.com>
2021-03-24 21:15:15 -0500

    cloud_app slight code cleanup

--- src/jumpCloudService.js
+++ src/jumpCloudService.js
@@ -1,53 +1,40 @@
-import axios from 'axios';
+import axios from "axios";
 export class JumpCloudService {
-    constructor() {
-      this.instance = axios.create({
-        baseURL: '/api',
-        headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-          }
-        });
-    }
-    async getUsers() {
-       const response  = await this.instance.get('/systemusers');
-       return response.data.results;
-    }
-    async newUser(user) {
-      const {
-        firstname,
-        lastname,
-        username,
-        email,
-      } = user;
-      const newUser = {
-        firstname,
-        lastname,
-        username,
-        email
-      };
-      const response  = await this.instance.post('/systemusers', newUser);
-      return response.data.results;
-   }
-   async editUser(user) {
-    const {
-      firstname,
-      lastname,
-      username,
-      email,
-      _id
-    } = user;
-    const editedUser = {
-      firstname,
-      lastname,
-      username,
-      email
-    };
-    const response  = await this.instance.put(`/systemusers/${_id}`, editedUser);
-    return response.data.results;
- }
- async deleteUser(deletedUserId) {
-  const response  = await this.instance.delete(`/systemusers/${deletedUserId}`)
-  return response.data.results;
-}
+  constructor() {
+    this.instance = axios.create({
+      baseURL: "/api",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
   }
+  async getUsers() {
+    const response = await this.instance.get("/systemusers");
+    return response.data.results;
+  }
+  async newUser(user) {
+    const response = await this.instance.post("/systemusers", {
+      firstname: user.firstname,
+      lastname: user.lastname,
+      username: user.username,
+      email: user.email,
+    });
+    return response.data.results;
+  }
+  async editUser(user) {
+    const response = await this.instance.put(`/systemusers/${user._id}`, {
+      firstname: user.firstname,
+      lastname: user.lastname,
+      username: user.username,
+      email: user.email,
+    });
+    return response.data.results;
+  }
+  async deleteUser(deletedUserId) {
+    const response = await this.instance.delete(
+      `/systemusers/${deletedUserId}`
+    );
+    return response.data.results;
+  }
+}
