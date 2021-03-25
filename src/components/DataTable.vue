085dc3e71225c00ef4a686ce124928b700f04dd3
David Schwartz <dbschwartz2@gmail.com>
2021-03-24 21:05:45 -0500

    cloud_app error handling added

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,40 +1,52 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="users"
-    sort-by="email"
-    class="elevation-1"
-  >
-    <template v-slot:top>
-      <v-toolbar flat>
-        <v-toolbar-title>Cloud Users</v-toolbar-title>
-        <v-divider class="mx-4" inset vertical></v-divider>
-        <v-spacer></v-spacer>
-        <create-or-edit-user
-          :inEditMode="inEditMode"
-          :editedItem="editedItem"
-          @save="save"
-          @reset="reset"
-        />
-        <delete-user
-         :dialogDelete="dialogDelete" 
-         @delete-item="deleteItem"          
-        />
-      </v-toolbar>
-    </template>
-    <template v-slot:item.actions="{ item }">
-      <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
-      <v-icon small @click="deleteDialogFn(item)"> mdi-delete </v-icon>
-    </template>
-    <template v-slot:no-data>
-      <v-btn color="primary" @click="getUsers"> Reset </v-btn>
-    </template>
-  </v-data-table>
+  <div>
+    <v-alert
+      v-model="alert"
+      color="red"
+      dense
+      dismissible
+      elevation="9"
+      outlined
+      text
+      type="error"
+      >
+{{ alertText }}</v-alert>
+    <v-data-table
+      :headers="headers"
+      :items="users"
+      sort-by="email"
+      class="elevation-1"
+    >
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>Cloud Users</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <create-or-edit-user
+            :inEditMode="inEditMode"
+            :editedItem="editedItem"
+            @save="save"
+            @reset="reset"
+          />
+          <delete-user :dialogDelete="dialogDelete" @delete-item="deleteItem" />
+        </v-toolbar>
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <v-icon small class="mr-2" @click="editDialogFN(item)">
+          mdi-pencil
+        </v-icon>
+        <v-icon small @click="deleteDialogFn(item)"> mdi-delete </v-icon>
+      </template>
+      <template v-slot:no-data>
+        <v-btn color="primary" @click="getUsers"> Reset </v-btn>
+      </template>
+    </v-data-table>
+  </div>
 </template>
 
 <script>
 import CreateOrEditUser from "./CreateOrEditUser.vue";
-import DeleteUser from './DeleteUser.vue';
+import DeleteUser from "./DeleteUser.vue";
 export default {
   name: "DataTable",
   data: () => ({
@@ -58,62 +70,64 @@ export default {
       email: "",
     },
     inEditMode: false,
-    dialogDelete: false
+    dialogDelete: false,
+    alert: false,
+    alertText: "",
   }),
   components: { CreateOrEditUser, DeleteUser },
   created() {
     this.getUsers();
   },
   methods: {
-    async getUsers() {
-      this.users = await this.$jCloudService.getUsers();
-    },
-    async handleNewUser(newUser) {
-      try {
-        await this.$jCloudService.newUser(newUser);
-        this.getUsers();
-      } catch (error) {
-        console.log(error);
-      }
-    },
     reset() {
       this.editedItem = Object.assign({}, this.defaultItem);
       this.editedIndex = -1;
       this.inEditMode = false;
     },
-    editItem(item) {
+    editDialogFN(item) {
       this.editedIndex = this.users.indexOf(item);
       this.editedItem = Object.assign({}, item);
       this.inEditMode = true;
     },
-    async deleteItem (flag) {
-        this.dialogDelete = false
-        if (flag === "confirm") {
-          await this.$jCloudService.deleteUser(this.editedItem._id);
-          await this.getUsers();
-        }
-        this.$nextTick(() => {
-          this.reset();
-        })
-      },
-    deleteDialogFn (item) {
-        this.editedIndex = this.users.indexOf(item)
-        this.editedItem = Object.assign({}, item)
-        this.dialogDelete = true
-      },
+    deleteDialogFn(item) {
+      this.editedIndex = this.users.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true;
+    },
+    handleError(err) {
+      this.alert = true;
+      this.alertText = err.response.data.message;
+    },
+    async getUsers() {
+      this.users = await this.$jCloudService.getUsers();
+    },
     async save() {
       try {
         if (this.editedIndex > -1) {
-           await this.$jCloudService.editUser(this.editedItem)
+          await this.$jCloudService.editUser(this.editedItem);
         } else {
           await this.$jCloudService.newUser(this.editedItem);
         }
       } catch (error) {
-        console.log(error);
+        this.handleError(error);
       }
       this.inEditMode = false;
       this.getUsers();
     },
-  },
+    async deleteItem(flag) {
+      this.dialogDelete = false;
+      if (flag === "confirm") {
+        try{
+         await this.$jCloudService.deleteUser(this.editedItem._id);
+         await this.getUsers();
+        } catch (error) {
+          this.handleError(error)
+        }
+      }
+      this.$nextTick(() => {
+        this.reset();
+      });
+    }
+  }
 };
 </script>
